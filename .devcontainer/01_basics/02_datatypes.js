8dfc1c504b8ba61dbb21855c166fcf43c1bda96f
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-01 14:12:30 +0000

    02 datatype done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)   // we re using nodejs
+console.log(3+3);
+ let name = "abhishek"
+ let age = 18
+ let isLoggedIn = false
+const i = 4646163
+console.table([name , age, isLoggedIn , i]);
+
+ // number => 2 to ki power 53
+ // bigint
+ // string => true/false
+ // null
+
+ // Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+let a = "16" + "Volvo";
+let b = 16 + "Volvo";
+
+console.table([name , age, isLoggedIn , i , x ,y ,person , cars, date,color, lastName ,length,weight , a , b]);
