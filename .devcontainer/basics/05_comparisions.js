157f469bc3f322a840029ec3a7a147c63a60a601
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-02 10:55:23 +0000

    05_comparision done

---
+++ .devcontainer/basics/05_comparisions.js
@@ -0,0 +1,21 @@
+console.log(2>1);//true
+console.log(2>=1);//true
+console.log(2<1);//false
+console.log(2==1);//false
+console.log(2!=1);//true
+//conversion chech
+console.log("2">1);//true
+console.log("02">1);//true
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+console.log(undefined == 0);//f
+console.log(undefined > 0);//f
+console.log(undefined < 0);//f
+
+//equality check with data type
+console.log("2" === 2);//f
+
+
+
