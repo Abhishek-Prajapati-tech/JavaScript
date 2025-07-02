9fe5c5939f5c05458184460571b2b23ca9f71dd9
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-02 01:37:19 +0000

    01_ variable is done

---
+++ .devcontainer/basics/01_variable.js
@@ -0,0 +1,16 @@
+
+const AccuntId = 12345678
+let AcccountEmail = "Abhi7470@gmail.com"
+var Accountpossword = "123456"
+accountCity = "bhopal"
+console.log([AcccountEmail, Accountpossword, AccuntId, accountCity]);
+// accountId = 2 // not allow
+
+
+//  prefer not to use var 
+//  because of issue in block scope and functional scope
+AcccountEmail = "Abhi8085@gmail.com"
+Accountpossword = "789456"
+accountCity = "indore"
+
+console.log([AcccountEmail, Accountpossword, AccuntId, accountCity]);
