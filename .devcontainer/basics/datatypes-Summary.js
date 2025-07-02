5f62277fdb6ae82ae6d55b59f34d5d70826edb2d
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-02 11:13:47 +0000

    05_datatypes done

---
+++ .devcontainer/basics/datatypes-Summary.js
@@ -0,0 +1,18 @@
+//data type ko kis tarike se memory me rakha jata he or acceses kiya jata he
+//data type ki 2 catagory he
+// 1 => primitive 2 => Nonprimitive{referancing}
+
+//JavaScript is a dynemuc type language
+//1=> primitive 
+// 7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+const score = 100//Number
+const value = 103.52//Number
+
+const isloggedIn = false//Boolean
+const temp = null//null
+let undefined;//undefined
+
+const id = Symbol('123')//Symbole
+
+//2 => referance{Non primitive}
+// Array. object, Functions
