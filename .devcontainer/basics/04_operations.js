6ac482d60e1d2cded1bc5382986602cd24f75042
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-02 10:39:46 +0000

    04_operation done

--- .devcontainer/basics/04_operations.js
+++ .devcontainer/basics/04_operations.js
@@ -20,4 +20,18 @@ console.log("1" + 2 + 2);//122
 console.log(1 + 2 + "2");//32
 console.log(3 + 4 * 5 % 3);//5
 
+console.log(+true);//1
+//console.log(true+);// not allow
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2;
+console.log([num1 , num2. num3]);//4,4,4
+
+let gamecounter = 100
+++gamecounter//gamecounter++ = 101
+console.log(gamecounter);//101
+
+
+
+
 
