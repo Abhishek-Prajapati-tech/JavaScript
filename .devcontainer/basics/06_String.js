275d228edd4cb4bd647161c0e00744322025022d
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-18 16:52:34 +0000

    string done

---
+++ .devcontainer/basics/06_String.js
@@ -0,0 +1,11 @@
+const name = "abhishek"
+const repoCount = 80
+
+ console.log(name + repoCount + " value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Abhishek')
+
+console.log(gameName[0]);
+
