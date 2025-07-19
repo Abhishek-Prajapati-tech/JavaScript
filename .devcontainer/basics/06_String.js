73d2c16f394f3322a1b4be23bf536046f8c7f5e7
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-19 06:12:28 +0000

    string done

--- .devcontainer/basics/06_String.js
+++ .devcontainer/basics/06_String.js
@@ -5,7 +5,30 @@ const repoCount = 80
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Abhishek')
+const gameName = new String('A b h i sh ek')
 
 console.log(gameName[0]);
+console.log(gameName.charAt(2));
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const newString1 = gameName.slice(-8,4)
+console.log(newString1);
+
+const newString2 = "  Abhishek prajapati "
+console.log(newString2);
+console.log(newString2.trim());
+
+const url = "https://www.bingdigfinoehe"
+console.log(url.replace(".", '-'));
+
+console.log(url.includes('www'));
+console.log(gameName.split(" "));
+
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+let word = myArray[1];
+console.log(word);
+console.log(myArray[0]);
 
