8e3f9aa81beb7625951372617444dd9a1559276b
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-20 14:22:11 +0000

    object done

--- .devcontainer/basics/11_objects.js
+++ .devcontainer/basics/11_objects.js
@@ -1,16 +1,18 @@
-// singleton
-
-//// object litrals
+// singleton hamesha constructor se banega
+//
+//// object litrals se ye singleton nhi (banta) hota iske multiple instensec ban jate he
 
 //Object.create
 
+// object litrals
 const mysym = Symbol("key1")
 const mysym1 = Symbol("key1")
 
 const user = {
     name: "Abhishek",// dote(.) se acces kar sakte he ids value lp
     "fullname": "Abhishek prajapati", // is ko (.) se access kar sakte he
-    [mysym]: "mykey1",
+    [mysym]: "mykey1",//symbolekey jab tak squre breket lage ager breket hatag to normal key hogi
+    
     age: 10,
     location: "bhopal",
     email: "Abhishekrajapati7470@gmail.com",
@@ -24,10 +26,22 @@ console.log(typeof(user["mysym"]));
 console.log(user[mysym]);
 
 user.email = "Abhiprajapati7470@gmail.com"
-Object.freeze(user)
+// Object.freeze(user)
 user.email = "Abhip7470@gmail.com"
 console.log(user);
 
 // functions
 
-user.gree
+user.greeting = function(){
+    console.log("hello abhishek");
+    
+}
+console.log(user.greeting);
+
+user.greeting1 = function(){
+    console.log(`hello abhishek, ${this.name}`);
+    
+}
+// console.log(user.greeting);
+console.log(user.greeting());
+console.log(user.greeting1());
