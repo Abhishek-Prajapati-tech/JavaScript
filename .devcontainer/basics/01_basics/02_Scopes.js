15606263bbba27cca84014cfd4942b53b58a74f2
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-21 10:21:57 +0000

    done

--- .devcontainer/basics/01_basics/02_Scopes.js
+++ .devcontainer/basics/01_basics/02_Scopes.js
@@ -18,3 +18,40 @@ console.log("INNER: ", a);
 // console.log(a);
 // console.log(b);
 console.log(a);
+
+function one(){
+    const username = "Abhishek"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        // chiled function(two) parent function(one) ke data ko
+        //  accecekar sakte he isliye isk closure bhi bolte he
+    }
+    //console.log(website);
+   two()
+}
+one()
+
+if (true) {
+    const username = "Abhishek"
+    if (username == "Abhishek") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    //console.log(website); function ke bhar he
+}
+//console.log(username);//ye bhi nhi hoga
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+// console.log(addone(5))
+
+const addtwo = function (num){
+    return num + 2
+}
+console.log(addtwo(8));
+
