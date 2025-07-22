b06e4ff2f56f78b6307ef2d4d7e965db9f628073
Abhishek Prajapati <abhishekprajapati7470@gmail.com>
2025-07-22 06:35:40 +0000

    don

---
+++ .devcontainer/basics/01_basics/05_Excution.js
@@ -0,0 +1,11 @@
+// +++ javaScript Excution contExt +++
+// 1 global Excution contExt
+// 2 function Excution contExt
+// 3 Eval Excution contExt
+
+// {}mmory cration phase
+ let val1 = 10
+ let val2 = 20
+ function addnum(val1,val2) {
+    
+ }
